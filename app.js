b3225373868375417a632236862a786a737f7054
Vishnu <vishnu_venugopal@live.com>
2017-07-24 19:01:25 +0000

    Created user model and configured passport #3

--- app.js
+++ app.js
@@ -2,21 +2,36 @@
     var mongoose                = require('mongoose');
     var passport                = require('passport');
     var bodyParser              = require('body-parser');
+    var User                    = require('./models/user');
     var LocalStrategy           = require('passport-local');
     var passportLocalMongoose   = require('passport-local-mongoose')
     
-    mongoose.connect('mongodb://localhost/auth_demo_app');
+    mongoose.connect('mongodb://localhost/auth_demo_app', {useMongoClient: true});
+    
+
     
     var app = express();
     app.set('view engine', 'ejs');
     
+    app.use(require('express-session')({
+        secret: 'J + D if you know what I mean',
+        resave: false,
+        saveUnitialized: false
+    }));
+    
+    app.use(passport.intialize());
+    app.use(passport.session());
+    
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+    
     app.get('/', function(req, res){
         res.render('home');
-    })
+    });
     
     app.get('/secret', function(req, res){
         res.render('secret');
-    })
+    });
     
     app.listen(process.env.PORT, process.env.IP, function(){
         console.log('server started...');
